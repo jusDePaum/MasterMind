1e30877596d1c7a947a9b53ed13efcae0e98626e
Jonathan <jonathan.longeville@gmail.com>
2024-10-15 10:40:17 +0200

    Autoloader

--- autoLoader.php
+++ autoLoader.php
@@ -6,6 +6,10 @@ function autoLoader($class): void
         $class = substr($class, strlen("Model_"));
         include_once "Model/$class.php";
     }
+    else if(str_starts_with($class, "Repository_")){
+        $class = substr($class, strlen("Repository_"));
+        include_once "Repository/$class.php";
+    }
 }
 
 spl_autoload_register("autoLoader");
